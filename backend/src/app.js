ec7f0fdd2b0f1895cfd8d29d324bd1e979546233
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-08-09 15:13:58 +0100

    new routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 
 import userRoutes from './routes/userRoutes.js';
 import audioRoutes from './routes/audioRoutes.js';
+import iconRoutes from './routes/iconRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -20,6 +22,8 @@ app.use('/uploads', express.static('uploads'));
 
 app.use('/api/users', userRoutes);
 app.use('/api/audio', audioRoutes);
+app.use('/api/icon', iconRoutes);
+app.use('api/auth', authRoutes);
 
 
 export default app;
