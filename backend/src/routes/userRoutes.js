014e64cc7b01ce10cf65a6a448b2e3cb4cc21d1c
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-08-09 15:12:34 +0100

    profilepicture upload change

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from 'express';
-import { register, login, logout, getUserProfile, uploadProfilePicture, changePassword, sendVerificationEmail, verifyEmail, getOtherUserProfile } from '../controllers/userController.js';
+import { 
+  register, 
+  login, 
+  logout, 
+  
+  getUserProfile, 
+  changePassword,
+
+  sendVerificationEmail, 
+  verifyEmail 
+} from '../controllers/userController.js';
 import authenticateToken from '../middleware/auth.js';
-import upload from '../middleware/upload.js';
+
 
 const router = express.Router();
 
@@ -12,14 +22,13 @@ router.post('/logout', authenticateToken, logout);
 // it first executes authenticateToken then getUserProfile so in auth we decode the token to id of user
 router.get('/me', authenticateToken, getUserProfile);
 
-router.put('/upload-profile-picture', authenticateToken, upload.single('profilePicture'), uploadProfilePicture);
+//router.put('/upload-profile-picture', authenticateToken, upload.single('profilePicture'), saveProfilePicture);
+router.put('/upload-profile-picture', authenticateToken, handleProfilePictureUpload, saveProfilePicture);
+
 router.put('/change-password', authenticateToken, changePassword);
 
 router.post('/send-verification', authenticateToken, sendVerificationEmail);
 router.get('/verify-email/:token', verifyEmail);
-
-//router.get('/:userId', authenticateToken, getOtherUserProfile);
-//router.get('/:userId/trips', authenticateToken, getUserTrips);
 
 
 export default router;
