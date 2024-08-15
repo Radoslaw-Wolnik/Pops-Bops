7cec85a8398b01d488f704a3990b83b0eef0d183
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-08-15 12:22:38 +0100

    deprecieted but updated to cookies (prev token in header)

--- backend/src/utils/tokenExtractor.js
+++ backend/src/utils/tokenExtractor.js
@@ -1,7 +1,9 @@
 // utils/tokenExtractor.js
 const extractToken = (req) => {
-    const authHeader = req.headers['authorization'];
-    return authHeader && authHeader.split(' ')[1];
+    //const authHeader = req.headers['authorization'];
+    //return authHeader && authHeader.split(' ')[1];
+    const token = req.cookies.token;
+    return token
 };
 
 export default extractToken;
