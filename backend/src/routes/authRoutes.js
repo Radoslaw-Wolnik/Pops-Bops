1edaeb1e4f6a8a7b072fcd9cd160b60058e92e14
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-08-15 12:22:12 +0100

    changed login logoutr register verify from user to auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,30 @@
 // src/routes/authRoutes.js
 import express from 'express';
-import { refreshToken } from '../controllers/authController.js';
-import authenticateToken from '../middleware/auth.js';
+import { 
+    register, 
+    login, 
+    logout, 
+    refreshToken, 
+    
+    sendVerificationEmail, 
+    verifyEmail, 
+    
+    changePassword
+} from '../controllers/authController.js';
+import { authenticateToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.post('/register', register);
+
+router.post('/login', login);
+router.post('/logout', authenticateToken, logout);
+
 router.post('/refresh-token', authenticateToken, refreshToken);
+
+router.post('/send-verification', authenticateToken, sendVerificationEmail);
+router.get('/verify-email/:token', verifyEmail);
+
+router.put('/change-password', authenticateToken, changePassword);
 
 export default router;
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,26 +1,14 @@
 import express from 'express';
 import { 
-  register, 
-  login, 
-  logout, 
-  
   getUserOwnProfile, 
-  changePassword,
 
   handleProfilePictureUpload,
   saveProfilePicture,
-
-  sendVerificationEmail, 
-  verifyEmail 
 } from '../controllers/userController.js';
-import authenticateToken from '../middleware/auth.js';
+import  { authenticateToken } from '../middleware/auth.js';
 
 
 const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', authenticateToken, logout);
 
 // it first executes authenticateToken then getUserProfile so in auth we decode the token to id of user
 router.get('/me', authenticateToken, getUserOwnProfile);
@@ -28,10 +16,6 @@ router.get('/me', authenticateToken, getUserOwnProfile);
 //router.put('/upload-profile-picture', authenticateToken, upload.single('profilePicture'), saveProfilePicture);
 router.put('/upload-profile-picture', authenticateToken, handleProfilePictureUpload, saveProfilePicture);
 
-router.put('/change-password', authenticateToken, changePassword);
-
-router.post('/send-verification', authenticateToken, sendVerificationEmail);
-router.get('/verify-email/:token', verifyEmail);
 
 
 export default router;
