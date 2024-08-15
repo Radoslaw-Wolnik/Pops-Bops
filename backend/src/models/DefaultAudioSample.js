724845cc4e60363bf9ac1cd8e950651613f1209c
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-08-15 16:17:30 +0100

    imported moongoose -.-

--- backend/src/models/DefaultAudioSample.js
+++ backend/src/models/DefaultAudioSample.js
@@ -1,5 +1,6 @@
 // models/DefaultAudioSample.js
 import { AudioSample } from './AudioSample.js';
+import mongoose from 'mongoose';
 
 const DefaultAudioSample = AudioSample.discriminator('DefaultAudioSample', new mongoose.Schema({
   forMainPage: { type: Boolean, default: false }
--- backend/src/models/UserAudioSample.js
+++ backend/src/models/UserAudioSample.js
@@ -1,5 +1,6 @@
 // models/UserAudioSample.js
 import { AudioSample } from './AudioSample.js';
+import mongoose from 'mongoose';
 
 const UserAudioSample = AudioSample.discriminator('UserAudioSample', new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
