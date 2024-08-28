4079ad2ed5a51bde77d8be113b86c2c0e3725b7b
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-08-28 13:25:23 +0100

    deleted deprecieted files

--- backend/src/utils/tokenExtractor.js
+++
@@ -1,9 +0,0 @@
-// utils/tokenExtractor.js
-const extractToken = (req) => {
-    //const authHeader = req.headers['authorization'];
-    //return authHeader && authHeader.split(' ')[1];
-    const token = req.cookies.token;
-    return token
-};
-
-export default extractToken;
