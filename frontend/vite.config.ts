cb8a89d5960920f9ee09b03c7a56e75f853dcbcb
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-08-30 15:56:01 +0100

    vite config without ssl - reverse proxy now for that

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import fs from 'node:fs'
-import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +9,6 @@ export default defineConfig({
     sourcemap: true  // Ensure source maps are generated
   },
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'ssl/cert/private-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/cert/certificate.pem')),
-    },
     host: true, // This makes the server accessible externally
     port: 5173, // Optional: specify the port
     watch: {
@@ -25,5 +19,3 @@ export default defineConfig({
     },
   }
 })
-
-
