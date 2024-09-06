ab96fdf76fb3dd108c1c88e946fcaa46f783374c
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-09-06 14:42:11 +0100

    first longer version

--- backend/src/utils/custom-errors.util.ts
+++ backend/src/utils/custom-errors.util.ts
@@ -1,11 +1,65 @@
-// src/utils/custom-error.util.ts
+// src/utils/custom-errors.util.ts
 
 export class CustomError extends Error {
-    statusCode: number;
-  
-    constructor(message: string, statusCode: number) {
-      super(message);
-      this.statusCode = statusCode;
-      Error.captureStackTrace(this, this.constructor);
-    }
+  constructor(public message: string, public statusCode: number) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
   }
+}
+
+export class NotFoundError extends CustomError {
+  constructor(resource: string) {
+    super(`${resource} not found`, 404);
+    this.name = 'NotFoundError';
+  }
+}
+
+export class UnauthorizedError extends CustomError {
+  constructor(message: string = 'Unauthorized access') {
+    super(message, 401);
+    this.name = 'UnauthorizedError';
+  }
+}
+
+export class ForbiddenError extends CustomError {
+  constructor(message: string = 'Access forbidden') {
+    super(message, 403);
+    this.name = 'ForbiddenError';
+  }
+}
+
+export class ValidationError extends CustomError {
+  constructor(message: string) {
+    super(message, 400);
+    this.name = 'ValidationError';
+  }
+}
+
+export class ConflictError extends CustomError {
+  constructor(message: string) {
+    super(message, 409);
+    this.name = 'ConflictError';
+  }
+}
+
+export class InternalServerError extends CustomError {
+  constructor(message: string = 'Internal server error') {
+    super(message, 500);
+    this.name = 'InternalServerError';
+  }
+}
+
+export class BadRequestError extends CustomError {
+  constructor(message: string) {
+    super(message, 400);
+    this.name = 'BadRequestError';
+  }
+}
+
+export class TooManyRequestsError extends CustomError {
+  constructor(message: string = 'Too many requests') {
+    super(message, 429);
+    this.name = 'TooManyRequestsError';
+  }
+}
