4c9f91835efc41df526275d1ca030460851b5f1a
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-09-06 14:42:48 +0100

    more consise revised verion

--- backend/src/utils/custom-errors.util.ts
+++ backend/src/utils/custom-errors.util.ts
@@ -11,55 +11,35 @@ export class CustomError extends Error {
 export class NotFoundError extends CustomError {
   constructor(resource: string) {
     super(`${resource} not found`, 404);
-    this.name = 'NotFoundError';
   }
 }
 
 export class UnauthorizedError extends CustomError {
   constructor(message: string = 'Unauthorized access') {
     super(message, 401);
-    this.name = 'UnauthorizedError';
   }
 }
 
 export class ForbiddenError extends CustomError {
   constructor(message: string = 'Access forbidden') {
     super(message, 403);
-    this.name = 'ForbiddenError';
   }
 }
 
 export class ValidationError extends CustomError {
   constructor(message: string) {
     super(message, 400);
-    this.name = 'ValidationError';
   }
 }
 
 export class ConflictError extends CustomError {
   constructor(message: string) {
     super(message, 409);
-    this.name = 'ConflictError';
   }
 }
 
 export class InternalServerError extends CustomError {
   constructor(message: string = 'Internal server error') {
     super(message, 500);
-    this.name = 'InternalServerError';
-  }
-}
-
-export class BadRequestError extends CustomError {
-  constructor(message: string) {
-    super(message, 400);
-    this.name = 'BadRequestError';
-  }
-}
-
-export class TooManyRequestsError extends CustomError {
-  constructor(message: string = 'Too many requests') {
-    super(message, 429);
-    this.name = 'TooManyRequestsError';
   }
 }
