032fb562127b3ecddfa79a808eddaa49d3ffb185
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-10-22 19:57:32 +0200

    deleted styles

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
-import { Button } from './Button';
-import { Input } from './Input';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
 
 export const RegisterPage: React.FC = () => {
   const [username, setUsername] = useState('');
